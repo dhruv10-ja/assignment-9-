b427c53459cf35549369169b44bb289e78e7cc3a
dhruv10-ja <1262252800@mitwpu.edu.in>
2026-02-04 15:05:13 +0530

    Add factorial calculation program in README.c
    Add a simple C program to calculate factorial.

--- README.c
+++ README.c
@@ -1,1 +1,18 @@
 # assignment-9-
+#include<stdio.h>
+int main()
+{
+int num;
+int i=1;
+int fact=1;
+
+printf("enter number:");
+scanf("%d",&num);
+for(i=1;i<5;i++)
+{
+    fact=fact*i;
+    
+}
+printf("%d",fact);
+return 0;      
+}
